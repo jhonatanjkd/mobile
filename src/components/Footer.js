0da4149b5feab7777c07ba3363a6b9a08c9ae0ed
jhonatan <jhonatanjkd86@gmail.com>
2019-10-24 00:20:04 -0500

    redes sociales

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -32,19 +32,51 @@ class Footer extends Component{
                        <ListaLinks listado={sobre} listName="Sobre Xcaret"/>
                        <ListaLinks listado={otros} listName="Otros"/>
                        <div className="custumer">
-                              <h2>Custumer service/ Salas:</h2>
-                              <p>Monday to Friday from 7:00 AM / Saturday and Sunday from 7:00 AM to 10:00 PM. lOCAL Time.</p>
-                              <b>Toll free number within Méxco</b>
-                              <div>
-                                    <a><span>Cancún</span> 998-883-3143</a>
-                                    <a></a>
+                              <h2 className = "text-white font-bold border-solid border-white border-b-2 mb-6" > Custumer service / Salas: </h2>
+                              <p className="text-white mb-6">Monday to Friday from 7:00 AM / Saturday and Sunday from 7:00 AM to 10:00 PM. lOCAL Time.</p>
+                              <b className="text-white mb-3 block">Toll free number within Méxco</b>
+                              <div className="flex justify-between mb-3">
+                                    <div className="w-1/2 pr-2">
+                                          <a className = "text-white block   text-center item-tel rounded-lg py-3 cursor-pointer"> <span className = "block" > Cancún: </span> 998-883-3143</a>
+                                    </div>
+                                     < div className = "w-1/2 pl-2" >
+                                          <a className = "text-white block  text-center item-tel rounded-lg py-3 cursor-pointer"> <span className = "block" > Playa del Carmen: </span> 984-206-0038</a>
+                                    </div>
+                                    
+                                    
                               </div>
-                              <b>Toll free number within Méxco</b>
-                              <div>
-                                    <a></a>
-                                    <a></a>
+                              <b className="text-white mb-3 block">Toll free number within Méxco</b>
+                              <div className="flex justify-between mb-3 flex-wrap">
+                                    <div className="w-1/2 pr-2 mb-3">
+                                           <a className="text-white block  text-center item-tel rounded-lg py-3 cursor-pointer"><span className="block">USA-CAN:</span> 1-855-326-0682</a>
+                                    </div>
+                                    <div className="w-1/2 pl-2 mb-3">
+                                          <a className="text-white block  text-center item-tel rounded-lg py-3 cursor-pointer"><span className="block">Brazil:</span> 0-800-892-3371</a>
+                                    </div>
+                                   <div className="w-1/2 pr-2 mb-3">
+                                          <a className="text-white block  text-center item-tel rounded-lg py-3 cursor-pointer"><span className="block">Argentina:</span> 0-800-892-3371</a>
+                                    </div>
+                                    <div className="w-1/2 pl-2 mb-3">
+                                          <a className="text-white block  text-center item-tel rounded-lg py-3 cursor-pointer"><span className="block">Colombia:</span> 01-800-952-0705</a>
+                                    </div>
+                                    <div className="w-1/2 pr-2 mb-3">
+                                          <a className="text-white block   text-center item-tel rounded-lg py-3 cursor-pointer"><span className="block">Spain:</span> 900-965-224</a>
+                                    </div>
+                                    <div className="w-1/2 pl-2 mb-3">
+                                          <a className="text-white block  text-center item-tel rounded-lg py-3 cursor-pointer"><span className="block">Chile:</span> 0-800-835-016</a>                      
+                                    </div>     
                               </div>
-                       </div>
+                              <img src="https://www.xcaret.com/img/reconocimientos_es.svg" className="w-full mb-4" />
+                              <div className="redes flex justify-around mb-4">
+                                    <a className="text-white text-3xl cursor-pointer hover:text-blue-700"><i class="fab fa-facebook-f"></i></a>
+                                    <a className="text-white text-3xl cursor-pointer hover:text-blue-400"><i class="fab fa-twitter"></i></a>
+                                    <a className="text-white text-3xl cursor-pointer hover:text-pink-800"><i class="fab fa-instagram"></i></a>
+                                    <a className="text-white text-3xl cursor-pointer hover:text-red-600"><i class="fab fa-youtube"></i></a>
+                              </div>
+                        </div>
+                        <p className="text-white mb-2">Xcaret - México, Chetumal - Puerto Juárez Federal Highway, Km. 282, Rancho Xcaret, Playa del Carmen, Quintana Roo México. CP 77710.</p>
+                        <p className="text-white mb-2">Teléfono Cancún: 998-883-3143 www.xcaret.com/en/ Open Monday to Sunday from 8:30 a.m. to 10:30 p.m.</p>
+                        <p className="text-white mb-2">Copyright 2019 Experiencias Xcaret Parques, S.A.P.I. de C.V</p>
                   </footer>
                 
             )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,14 +31,14 @@ class Home extends React.Component{
                     <TabsInfo tabs={tabsDetalles} tabType="tabdetalles"/>
                     <section className="cards px-8 mt-4">
                         <p className="text-center text-xl mb-2">Hasta 12 <b>meses</b> sin intereses</p> 
-                        <div className="flex sm:justify-between   justify-center">
-                            <img src="https://via.placeholder.com/40X32" className="w-1/6 mx-1"/>
-                            <img src="https://via.placeholder.com/40X32" className="w-1/6 mx-1"/>
-                            <img src="https://via.placeholder.com/40X32" className="w-1/6 mx-1"/>
-                            <img src="https://via.placeholder.com/40X32" className="w-1/6 mx-1"/>
-                            <img src="https://via.placeholder.com/40X32" className="w-1/6 mx-1"/>
-                            <img src="https://via.placeholder.com/40X32" className="w-1/6 mx-1"/>
-                            <img src="https://via.placeholder.com/40X32" className="w-1/6 mx-1"/>
+                        <div className="flex sm:justify-between justify-between mt-4 mb-4">
+                            <img src="https://www.xcaret.com/img/bancos/american.png" className=" mx-1"/>
+                            <img src="https://www.xcaret.com/img/bancos/visa.png" className=" mx-1"/>
+                            <img src="https://www.xcaret.com/img/bancos/mastercard.png" className=" mx-1"/>
+                            <img src="https://www.xcaret.com/img/bancos/discover.png" className=" mx-1"/>
+                            <img src="https://www.xcaret.com/img/bancos/diners.png" className=" mx-1"/>
+                            <img src="https://www.xcaret.com/img/bancos/paypal.png" className=" mx-1"/>
+                         
                         </div>
                         <p>Con los principales bancos y tarjetas emitidas en México.</p>
                         <p>*No aplica Paylpal <a href="#" className="font-bold">Más opciones <i className="fas fa-angle-double-right"></i></a></p>                       
